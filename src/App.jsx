2ab766c47524a41a401d5b1eeeb7f8cd696c27bd
AbdullahAlNaim <aanaim96@gmail.com>
2025-02-13 23:57:46 -0500

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Food from "./Food"
+import Section from "./Section"
 
 function App() {
     return (
       <>
         <Header />
+        <Section />
         <Food />
         <Food />
         <Footer />
---
+++ src/Section.jsx
@@ -0,0 +1,7 @@
+function Section() {
+    return (
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, iure atque ipsum ratione quaerat ab facere sunt incidunt vel voluptatem. Ab laboriosam fugit assumenda a enim tenetur ad voluptate architecto.</p>
+    );
+}
+
+export default Section
