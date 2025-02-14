7fc8f3e81b039c8953dae5b72aabc0997b494ece
Abdullah Naim <aanaim96@gmail.com>
2025-02-14 05:31:14 +0000

    working through github.dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./Header"
 import Footer from "./Footer"
 import Food from "./Food"
 import Section from "./Section"
+import Dev from "./Dev"
 
 function App() {
     return (
       <>
         <Header />
+        <Dev />
         <Section />
         <Food />
         <Food />
---
+++ src/Dev.jsx
@@ -0,0 +1,8 @@
+function Dev() {
+    const text = "Github.Dev Test"
+    return (
+        <h1>{text}</h1>
+    );
+}
+
+export default Dev
